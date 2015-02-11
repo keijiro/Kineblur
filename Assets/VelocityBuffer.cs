ea12724b618e0cf2435f32f37b9851dcb2f4bdfa
Keijiro Takahashi <keijiro@gmail.com>
2015-02-11 19:03:23 +0900

    Implemented the velocity buffer component.

--- Assets/VelocityBuffer.cs
+++ Assets/VelocityBuffer.cs
@@ -5,13 +5,44 @@ public class VelocityBuffer : MonoBehaviour
 {
     [SerializeField] Shader _shader;
 
+    RenderTexture _velocityBuffer;
+    GameObject _cloneObject;
+
     void OnEnable()
     {
-        GetComponent<Camera>().SetReplacementShader(_shader, null);
+        if (_cloneObject == null)
+        {
+            _cloneObject = new GameObject("Velocity Camera", typeof(Camera));
+            _cloneObject.camera.enabled = false;
+            _cloneObject.hideFlags = HideFlags.HideAndDontSave;
+        }
     }
 
     void OnDisable()
     {
-        GetComponent<Camera>().ResetReplacementShader();
+        if (_cloneObject != null) DestroyImmediate(_cloneObject);
+    }
+
+    void OnPreRender()
+    {
+        if (_velocityBuffer != null)
+            RenderTexture.ReleaseTemporary(_velocityBuffer);
+
+        _velocityBuffer = RenderTexture.GetTemporary((int)camera.pixelWidth, (int)camera.pixelHeight, 24, RenderTextureFormat.RGFloat);
+
+        var vc = _cloneObject.camera;
+        vc.CopyFrom(camera);
+        vc.backgroundColor = Color.black;
+        vc.clearFlags = CameraClearFlags.SolidColor;
+        vc.targetTexture = _velocityBuffer;
+        vc.RenderWithShader(_shader, null);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_velocityBuffer != null)
+            Graphics.Blit(_velocityBuffer, destination);
+        else
+            Graphics.Blit(source, destination);
     }
 }
