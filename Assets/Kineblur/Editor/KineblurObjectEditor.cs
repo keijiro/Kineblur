76f66dda3741943b71377b83612e3047f6c8ba1c
Keijiro Takahashi <keijiro@gmail.com>
2015-02-15 15:54:24 +0900

    Refactoring to KineblurOBject.

---
+++ Assets/Kineblur/Editor/KineblurObjectEditor.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(KineblurObject)), CanEditMultipleObjects]
+public class KineblurObjectEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+    }
+}
--- Assets/Kineblur/KineblurObject.cs
+++ Assets/Kineblur/KineblurObject.cs
@@ -1,27 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[AddComponentMenu("Kineblur/Rigid Object")]
+[AddComponentMenu("Kineblur/Kineblur Object")]
 public class KineblurObject : MonoBehaviour
 {
+    static int propertyID;
+
+    Renderer targetRenderer;
     Matrix4x4 previousMVP;
+    MaterialPropertyBlock propertyBlock;
+
+    Matrix4x4 CalculateMVP()
+    {
+        var mainCamera = Camera.main;
+
+        Matrix4x4 M = targetRenderer.localToWorldMatrix;
+        Matrix4x4 V = mainCamera.worldToCameraMatrix;
+        Matrix4x4 P = mainCamera.projectionMatrix;
+
+        return P * V * M;
+    }
+
+    void Awake()
+    {
+        propertyID = Shader.PropertyToID("_VelocityBuffer_MVP");
+        propertyBlock = new MaterialPropertyBlock();
+    }
 
     void Start()
     {
-        previousMVP = CalcMVP();
+        targetRenderer = GetComponent<Renderer>();
+        previousMVP = CalculateMVP();
     }
 
     void LateUpdate()
     {
-        renderer.material.SetMatrix("_VelocityBuffer_MVP", previousMVP);
-        previousMVP = CalcMVP();
-    }
+        propertyBlock.Clear();
+        propertyBlock.AddMatrix(propertyID, previousMVP);
+        targetRenderer.SetPropertyBlock(propertyBlock);
 
-    Matrix4x4 CalcMVP()
-    {
-        Matrix4x4 M = renderer.localToWorldMatrix;
-        Matrix4x4 V = Camera.main.worldToCameraMatrix;
-        Matrix4x4 P = Camera.main.projectionMatrix;
-        return P * V * M;
+        previousMVP = CalculateMVP();
     }
 }
