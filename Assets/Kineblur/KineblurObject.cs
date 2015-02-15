5e8c39d5d409467c23d9b2a53191911587952070
Keijiro Takahashi <keijiro@X1.local>
2015-02-15 09:15:15 +0900

    Fixed model-view matrix.

--- Assets/Kineblur/KineblurObject.cs
+++ Assets/Kineblur/KineblurObject.cs
@@ -19,7 +19,7 @@ public class KineblurObject : MonoBehaviour
 
     Matrix4x4 CalcMVP()
     {
-        Matrix4x4 M = transform.localToWorldMatrix;
+        Matrix4x4 M = renderer.localToWorldMatrix;
         Matrix4x4 V = Camera.main.worldToCameraMatrix;
         Matrix4x4 P = Camera.main.projectionMatrix;
         return P * V * M;
